f039764eab27a929a2dfa732430dbdb34122bbfc
dudgk05 <dudgk05@naver.com>
2022-04-01 17:36:30 +0900

    20220401, 2

--- 20220326/test1.cpp
+++ 20220326/test1.cpp
@@ -5,16 +5,16 @@
 #include <ctime>
 
 #define SIZE 20
-//점 저장장소
+
 float datax[SIZE] = { 0, };
 float datay[SIZE] = { 0, };
 
-// 점(datax, datay)와 직선 ax - y + b = 0 사이의 거리를 나타내는 식
+
 float dis(float datax, float datay, float a, float b) {
 	return abs(a * datax - datay + b) / sqrt(a * a + 1);
 }
 
-//경사하강법을 적용할 함수 
+ 
 float f(float a, float b) {
 	float sum = 0.0;
 	for (int i = 0; i < SIZE; i++) {
@@ -23,13 +23,12 @@ float f(float a, float b) {
 	return sum;
 }
 
-//기울기를 위한 미분함수
-//a에 대한 미분 함수
+
 float dfabda(float a, float b, float da) {
 	return (f(a + da, b) - f(a, b)) / da;
 }
 
-//b에 대한 미분 함수
+
 float dfabdb(float a, float b, float db) {
 	return (f(a, b + db) - f(a, b)) / db;
 }
@@ -41,7 +40,7 @@ float EE(float x0, float x1, float y0, float y1) {
 int main() {
 	srand((unsigned int)time(NULL));
 
-	//make data 20
+
 	for(int i = 0; i<SIZE; i++)
 	{
 		datax[i] = i + ((float)(rand() % 100)/100 - 0.5);
@@ -57,7 +56,7 @@ int main() {
 	float da = 0.01;
 	float db = 0.01;
 	
-	//y = ax + b, a = a1, b = b1
+
 	float a1 = 4, b1 = 3;
 	
 	while (EE(a0, b0, a1, b1) > 0.0001 && iteration < 100000) {
